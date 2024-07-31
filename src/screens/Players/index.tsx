8658633532ddf1258248e1efb446a2ddc7d5a5f3
RaissaMacedo <raissamacedo7@gmail.com>
2024-07-31 18:24:14 -0300

    feat: create component playerCard

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,10 +6,13 @@ import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
 import { FlatList } from "react-native";
 import { useState } from "react";
+import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Players() {
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState([]);
+  const [players, setPlayers] = useState(['Raíssa', 'Karol']);
   return (
     <Container>
       <Header showBackButton />
@@ -43,6 +46,30 @@ export function Players() {
           {players.length}
         </NumbersOfPlayers>
       </HeaderList>
+      <FlatList
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <PlayerCard 
+            name={item}
+            onRemove={() => {}}
+          />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message="Não há pessoas nesse time"
+          />
+        )}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={[
+          {paddingBottom: 100}, 
+          players.length === 0 && { flex: 1 }
+        ]}
+      />
+      <Button
+        title="Remover turma"
+        type="SECONDARY"
+      />
      
     </Container>
   )
